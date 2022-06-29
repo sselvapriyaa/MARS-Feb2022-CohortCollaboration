24c735a790be0481c786e107948b5361e0111719
sselvapriyaa <selvapriyaphd@gmail.com>
2022-06-29 18:43:06 -0500

    Selva: updated About.js

--- src/components/Selvapriya/About.js
+++ src/components/Selvapriya/About.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import SelvapriyaA from '../../images/SelvapriyaA.jpg'
-import 'AboutSelvapriya.css'
+//import 'AboutSelvapriya.css'
+import 'About.css'
 
 function About(props) {
     return (
         <div className = "text-center">
             <h1 className = "heading">  </h1>
-                <h5 >Hello, I'm</h5> 
-                <h1>Selvapriya</h1>
+                <h5 className = "heading">Hello, I'm</h5> 
+                <h1 className = "heading">Selvapriya</h1>
                 <h5>Fullstack Developer</h5>
                 
             <img src={SelvapriyaA} alt="picture of Selvapriya A "></img>
