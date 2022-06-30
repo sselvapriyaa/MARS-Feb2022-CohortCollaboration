592244af6c5acbe17eabea53252fc3d347720036
AnnieChris <annieesther3@gmail.com>
2022-06-30 16:28:21 -0500

    Annie's update for homepage cards&nav design works

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { HashRouter as Router, Link, Routes, Route } from "react-router-dom";
 // Bootstrap
 import {
   Card,
@@ -10,31 +11,23 @@ import {
   NavDropdown,
   Row,
 } from "react-bootstrap";
-import { HashRouter as Router, Link, Routes, Route } from "react-router-dom";
-// Personal Style
-import "../App.css";
 
+// Personal Style
 import "../components/styles.css";
 
-// temporary background photos
-import mountains1 from "../images/mountains1.jpg";
-import mountains2 from "../images/mountains2.jpg";
-import mountains3 from "../images/mountains3.jpg";
-import technology from "../images/technology.jpg";
-import woman_at_desktop from "../images/woman_at_desktop.jpg";
-import career_in_tech from "../images/career_in_tech.jpg";
-
+// carousel background photos
 import technology1 from "../images/technology1.jpg";
 import Banner_Image from "../images/Banner_Image.jpg";
 import Banner_Image1 from "../images/Banner_Image1.jpg";
 import Banner_Image2 from "../images/Banner_Image2.jpg";
 
+//Returners photos
 import ColtonW from "../images/ColtonW.jpg";
 import LynnR from "../images//LynnR.jpg";
 import RichardF from "../images/RichardF.jpg";
 import HeidiW from "../images/HeidiW.jpg";
-import NagaK from "../images/Naga_Priya_Img_2.png";
 import AnnieW from "../images/AnnieW.jpg";
+import NagaK from "../images/Naga_Priya_Img_2.png";
 import SelvapriyaA from "../images/SelvapriyaA.jpg";
 import Greeshma from "../images/Greeshma-PP.jpg";
 
@@ -67,10 +60,10 @@ function Home(props) {
             alt="Second slide"
           />
           <Carousel.Caption>
-            <h3 style={{ color: "black" }}>Second slide label</h3>
-            {/* {<p style={{ color: "black" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>} */}
+            <h3 style={{ color: "white" }}>MARS RETURNSHIP</h3>
+            {<p style={{ color: "white" }}>
+            Mars Returnship is a great platform to update our Professional development skills via remote training and also providing hands on experience in the latest technologies
+            </p>} 
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -80,10 +73,10 @@ function Home(props) {
             alt="Third slide"
           />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3  style={{ color: "black" }}>MARS RETURNSHIP</h3>
+            <p  style={{ color: "black" }}>
+            Mars Returnship focuses on reskilling and training to fill in the gaps and get the returner back on track.
+            </p> 
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -93,10 +86,10 @@ function Home(props) {
             alt="Fourth slide"
           />
           <Carousel.Caption>
-            <h3 style={{ color: "black" }}>Fourth slide label</h3>
-            {/* {<p style={{ color: "black" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>} */}
+            <h3 style={{ color: "black" }}>MARS RETURNSHIP</h3>
+            {<p style={{ color: "black" }}>
+            In Mars Returnship program we work collaboratively as a team to achieve our common goal. 
+            </p>} 
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -105,9 +98,9 @@ function Home(props) {
       {/* Personal Cards - Row 1 */}
       <Container>
         <Row xs={1} md={2} lg={4} className="g-4">
-          <Col>
-            <Link to="/Colton">
-              <Card>
+          {/* <Col>
+          <Card>
+            <Link to="/Colton">              
                 <Card.Img
                   className="rounded-circle"
                   variant="top"
@@ -117,26 +110,28 @@ function Home(props) {
                   <Card.Title>Colton</Card.Title>
                   <Card.Text>Short Description</Card.Text>
                 </Card.Body>
-              </Card>
+              
             </Link>
-          </Col>
+            </Card>
+          </Col> */}
+
           <Col>
             <Card>
-              <Card.Img className="rounded-circle" variant="top" src={LynnR} />
+            <Link to="/Lynn"><Card.Img className="rounded-circle" variant="top" src={LynnR} />
               <Card.Body>
-                <Link to="/Lynn">
                   <Card.Title>Lynn</Card.Title>
-                </Link>
                 <Card.Text>
                   Experience in medical imaging, telecom, STEM education and
                   project mangement
                 </Card.Text>
               </Card.Body>
+              </Link>
             </Card>
           </Col>
+
           <Col>
-            {/* <Link to="/YourComponentName"> */}
             <Card>
+            <Link to="/Richard">
               <Card.Img
                 className="rounded-circle"
                 variant="top"
@@ -148,23 +143,37 @@ function Home(props) {
                   Long time programmer who loves science and math.
                 </Card.Text>
               </Card.Body>
+              </Link>
             </Card>
-            {/* </Link> */}
           </Col>
+
           <Col>
-            {/* <Link to="/YourComponentName"> */}
             <Card>
+            <Link to="/Heidi">
               <Card.Img className="rounded-circle" variant="top" src={HeidiW} />
-              <Card.Body>
-                <Link to="/Heidi">
-                  <Card.Title>Heidi</Card.Title>
-                </Link>
+              <Card.Body>                
+                  <Card.Title>Heidi</Card.Title>                
                 <Card.Text>
                   Linguistics grad excited to transition to tech!
                 </Card.Text>
               </Card.Body>
+              </Link>
             </Card>
-            {/* </Link> */}
+          </Col>
+
+          <Col>
+            <Card>
+            <Link to="/Annie">
+              <Card.Img className="rounded-circle" variant="top" src={AnnieW} />
+              <Card.Body>                
+                  <Card.Title>Annie</Card.Title>                
+                <Card.Text>
+                  Worked as a Web Developer. Designed and Developed user
+                  friendly dynamic Webpages using PHP.
+                </Card.Text>
+              </Card.Body>
+              </Link>
+            </Card>
           </Col>
         </Row>
       </Container>
@@ -174,68 +183,55 @@ function Home(props) {
       {/* Personal Cards - Row 2 */}
       <Container>
         <Row xs={1} md={2} lg={4} className="g-4">
+          
+
           <Col>
             <Card>
-              <Card.Img className="rounded-circle" variant="top" src={AnnieW} />
-              <Card.Body>
-                <Link to="/Annie">
-                  <Card.Title>Annie</Card.Title>
-                </Link>
-                <Card.Text>
-                  Worked as a Web Developer. Designed and Developed user
-                  friendly dynamic Webpages using PHP.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
+            <Link to="/NagaPriya">
               <Card.Img className="rounded-circle" variant="top" src={NagaK} />
-              <Card.Body>
-                <Link to="/NagaPriya">
-                  <Card.Title>Naga Priya K</Card.Title>
-                </Link>
+              <Card.Body>                
+                  <Card.Title>Naga Priya K</Card.Title>                
                 <Card.Text>
                   Full Stack Intern, Experienced in Software Development and
                   Testing.
                 </Card.Text>
               </Card.Body>
+              </Link>
             </Card>
           </Col>
+
           <Col>
-            {/* <Link to="/YourComponentName"> */}
             <Card>
+            <Link to="/Selvapriya">
               <Card.Img
                 className="rounded-circle"
                 variant="top"
                 src={SelvapriyaA}
               />
               <Card.Body>
-                <Link to="/Selvapriya">
+                
                   <Card.Title>Selvapriya</Card.Title>
-                </Link>
+                
                 <Card.Text>Experience in full stack web development</Card.Text>
               </Card.Body>
+              </Link>
             </Card>
-            {/* </Link> */}
           </Col>
+
           <Col>
-            {/* <Link to="/YourComponentName"> */}
             <Card>
+            <Link to="/Greeshma">
               <Card.Img
                 className="rounded-circle"
                 variant="top"
                 src={Greeshma}
               />
               <Card.Body>
-              <Link to="/Greeshma">
-
-                <Card.Title>Greeshma</Card.Title>
-                </Link>
+              <Card.Title>Greeshma</Card.Title>            
                 <Card.Text>Worked as a Software Developer.</Card.Text>
               </Card.Body>
+              </Link>
             </Card>
-            {/* </Link> */}
           </Col>
         </Row>
       </Container>
