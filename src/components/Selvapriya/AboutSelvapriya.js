dafa4e146d46fa9d74678385b5220e53682e60d2
sselvapriyaa <selvapriyaphd@gmail.com>
2022-07-02 08:39:41 -0500

    Selva: updated About.css

--- src/components/Selvapriya/AboutSelvapriya.js
+++ src/components/Selvapriya/AboutSelvapriya.js
@@ -18,8 +18,8 @@ function AboutSelvapriya(props) {
           </div>
 
           <div className = "section2">    
-       I am a Full-Stack Web Developer with experience in HTML5, CSS3, JavaScript, React JS.
-       I am passionate about learning new programming languages. I enjoy working on collaborative teams.   
+       I am a Full-Stack Developer with experience in Java, React JS, MS SQL.
+       I have a Masters Degree in Computer Science. I am ready for a more challenging project.  
       </div>
 
       <div className = "section3">    
@@ -32,30 +32,29 @@ function AboutSelvapriya(props) {
         >Resume </a>
        
       </div>
-      </section>  
-      
-      <footer className = "social-mediaicons" >
+      </section>      
+      <footer className = "socialmedia" >   
       <p className = "icon1">
-        <a href = "https://www.linkedin.com/in/selvapriyaanandamirtharaj/" alt = "linkedin link"
-        target = "_blank"
-        >  
-        <i class="bi bi-linkedin"></i>
-          </a>     
-      </p>
-      <p className = "icon2">
         <a href = "" alt = "facebook link"
         target = "_blank"
         >  
         <i class="bi bi-facebook"></i>
-          </a>     
+          </a>    
       </p>
-      <p className = "icon3">
+      <p className = "icon2">
         <a href = "" alt = "instagram link"
         target = "_blank"
         > 
       <i class="bi bi-instagram"></i>
       </a>     
-      </p>     
+      </p>  
+      <p className = "icon3">
+        <a href = "https://www.linkedin.com/in/selvapriyaanandamirtharaj/" alt = "linkedin link"
+        target = "_blank"
+        >  
+        <i class="bi bi-linkedin"></i>
+          </a>   
+      </p>   
       <p className = "icon4">
         <a href = "https://github.com/sselvapriyaa" alt = "github link"
         target = "_blank"
@@ -63,11 +62,11 @@ function AboutSelvapriya(props) {
         <i class="bi bi-github"></i>
         </a> 
         </p>
-        
-      <p className = "copyright">
-      Copyright &copy; 2022
-        </p>  
         </footer>
+        <p className = "copyright">
+      Copyright &copy; 2022
+        </p> 
+       
     </div>
   
     );
