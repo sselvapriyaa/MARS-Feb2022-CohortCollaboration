1230f7fb83160da07fe8d043f395fb5c7db16d53
Heidi <heidi.willden@me.com>
2022-07-06 10:23:56 -0500

    Commented out sections

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ import { HashRouter as Router, Link, Routes, Route } from "react-router-dom";
 import "./App.css";
 // Returner Components
 import Home from "./components/Home";
-import Colton from "./components/Colton/Colton";
+// import Colton from "./components/Colton/Colton";
 import AboutLynn from "./components/Lynn/AboutLynn";
-import AboutRichard from "./components/Richard/AboutRichard";
+// import AboutRichard from "./components/Richard/AboutRichard";
 import AboutHeidi from "./components/Heidi/AboutHeidi";
 import AboutAnnie from "./components/Annie/AboutAnnie";
 import AboutNaga from "./components/NagaPriya/AboutNaga";
@@ -24,7 +24,6 @@ import AboutSelvapriya from "./components/Selvapriya/AboutSelvapriya";
 import AboutGreeshma from "./components/Greeshma/AboutGreeshma";
 
 function App() {
-
   return (
     <div className="App">
       <Router>
@@ -41,37 +40,32 @@ function App() {
                 </Nav.Link>
                 <NavDropdown
                   title="February 2022 Returners"
-                  id="basic-nav-dropdown">
+                  id="basic-nav-dropdown"
+                >
                   {/* <NavDropdown.Item href="./#/Colton">
                     Colton W
                   </NavDropdown.Item>
                   <NavDropdown.Divider /> */}
-                  <NavDropdown.Item href="./#/Lynn">
-                    Lynn R
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="./#/Lynn">Lynn R</NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="./#/Richard">
+                  {/* <NavDropdown.Item href="./#/Richard">
                       Richard
                     </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                  <NavDropdown.Item href="./#/Heidi">
-                    Heidi W
-                  </NavDropdown.Item>
+                    <NavDropdown.Divider /> */}
+                  <NavDropdown.Item href="./#/Heidi">Heidi W</NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="./#/Annie">
-                    Annie W
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="./#/Annie">Annie W</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="./#/NagaPriya">
                     Naga Priya
-                    </NavDropdown.Item>
+                  </NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="./#/Selvapriya">
-                     Selvapriya A 
+                    Selvapriya A
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="./#/Greeshma">
-                     Greeshma
+                    Greeshma
                   </NavDropdown.Item>
                   {/*<NavDropdown.Divider /> 
                   <NavDropdown.Item href="">Another Returner</NavDropdown.Item> */}
@@ -85,17 +79,17 @@ function App() {
           {/* To implement YOUR component, copy what I did here for the names of your own component */}
           {/* I named my component 'Colton', but feel free name it whatever you like! */}
           {/* DONT FORGET TO EDIT THE LINK ELEMENT NEAR YOUR CARD AS WELL */}
-          
+
           <Route path="*" element={<Home />} />
           <Route path="/home" element={<Home />} />
           {/* <Route path="/Colton" element={<Colton />} /> */}
           <Route path="/Lynn" element={<AboutLynn />} />
-          <Route path="/Richard" element={<AboutRichard />} />
-          <Route path="/Heidi" element={<AboutHeidi />}/>
-          <Route path='/Annie' element={<AboutAnnie />} /> 
-          <Route path='/Selvapriya' element={<AboutSelvapriya />} /> 
-          <Route path="/NagaPriya" element={<AboutNaga/>} />
-          <Route path="/Greeshma" element={<AboutGreeshma/>} />
+          {/* <Route path="/Richard" element={<AboutRichard />} /> */}
+          <Route path="/Heidi" element={<AboutHeidi />} />
+          <Route path="/Annie" element={<AboutAnnie />} />
+          <Route path="/Selvapriya" element={<AboutSelvapriya />} />
+          <Route path="/NagaPriya" element={<AboutNaga />} />
+          <Route path="/Greeshma" element={<AboutGreeshma />} />
           {/* <Route path='/ReturnerName' element={<ReturnerComponent />} /> */}
         </Routes>
       </Router>
